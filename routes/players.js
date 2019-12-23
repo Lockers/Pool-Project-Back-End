fd6b0743f8659cddd0326e275d075c39c5eaeace
Matt Locklin <mattlocklin@live.co.uk>
2019-12-23 13:59:01 +0000

    added get matches

--- routes/players.js
+++ routes/players.js
@@ -8,18 +8,16 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/:id').get((req, res) => {
-    console.log(req.params.id)
     Player.find({ leaguePosition: req.params.id })
         .then(player => res.json(player))
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/league-table').get((req, res) => {
-    Player.find().sort({ leaguePosition })
-        .then(position => res.json(position))
-        .catch(err => res.status(400).json('Error: ' + err))
-
-})
+router.route('/').get((req, res) => {
+    Player.find().sort({leaguePosition: 'asc'})
+        .then(player => res.json(player))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
 router.route('/').post((req, res) => {
     const newPlayer = new Player(req.body)
@@ -27,11 +25,11 @@ router.route('/').post((req, res) => {
 
         .then(() => res.json('Player Added'))
         .catch(err => res.status(400).json('Error: ' + err))
-})
+});
 
 router.route('/:id').put((req, res) => {
     Player.findByIdAndUpdate({ _id: req.params.id }, req.body)
         .then(() => res.json('Player Updated'))
         .catch(err => res.status(400).json('Error: ' + err))
-})
+});
 module.exports = router;
