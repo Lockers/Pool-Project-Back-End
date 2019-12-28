413422949e4547982163a7899e833a139d6cb129
Matt Locklin <mattlocklin@live.co.uk>
2019-12-28 18:12:10 +0000

    added results to update player

--- routes/results.js
+++ routes/results.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 let Results = require('../models/results.model');
-let Challenges = require('../models/challenges.model');
+let Player = require('../models/players.model');
 
 router.route('/').get((req, res) => {
     Results.find()
@@ -9,10 +9,18 @@ router.route('/').get((req, res) => {
 });
 
 router.route('/').post((req, res) => {
-    const newResult = new Results(req.body);
-    newResult.save()
-        .then(() => res.json('Result Added'))
-        .catch(err => res.status(400).json('Error: ' + err))
-})
+    if (req.body.challengerScore > req.body.challengedScore) {
+        console.log('Happy Path')
+        console.log(req.body.challenger)
+        Player.updateOne({ name: req.body.challenger }, {results: req.body}, { $inc: { played: + 1, won: + 1, totalPrizeMoney: + req.body.pot } })
+            .then(response => console.log(response))
+            .then(res => Player.updateOne({ name: req.body.challenged }, { $inc: { played: + 1, lost: + 1, totalPrizeMoney: - req.body.pot } }))
+        .then(response => console.log(response))
+    }
+    Results.create(req.body)
+    .then(() => res.json('Result Added'))
+    .catch(err => res.status(400).json('Error: ' + err))
+   
+});
 
 module.exports = router;
