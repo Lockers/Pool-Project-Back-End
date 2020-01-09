52d7ef7da6f8c6b102251a5124e9e97fae7b1534
Matt Locklin <mattlocklin@live.co.uk>
2020-01-09 15:21:34 +0000

    Sort results by date

--- routes/players.js
+++ routes/players.js
@@ -4,7 +4,7 @@ let Player = require('../models/players.model');
 
 router.route('/').get((req, res) => {
 
-    Player.find().sort({ leaguePosition: 'desc' })
+    Player.find().sort({ leaguePosition: 'asc' })
         .then(player => res.json(player))
         .catch(err => res.status(400).json('Error: ' + err));
 });
