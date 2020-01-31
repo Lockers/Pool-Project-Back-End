04454035c44c4fb0d9800677ad349d117386595b
Matt Locklin <mattlocklin@live.co.uk>
2020-01-31 16:46:56 +0000

    refactored players route to use Controllers

---
+++ controllers/playersController.js
@@ -0,0 +1,24 @@
+const Player = require('../models/players.model');
+
+exports.getPlayers = (req, res, next) => {
+    Player.find().sort({ leaguePosition: 'desc' })
+        .then(Player => res.json(Player))
+        .catch(err => res.status(400).json('Error: ' + err))
+}
+
+exports.getPlayerById = (req, res, next) => {
+    Player.findById(req.params.id)
+        .then(player => res.json(player))
+        .catch(err => res.status(400).json('Error: ' + err));
+}
+
+exports.addPlayer = (req, res, next) => {
+    Player.updateMany(
+        { leaguePosition: { $gte: req.body.leaguePosition } },
+        { $inc: { leaguePosition: + 1 } })
+        .then(res => {
+            return Player.create(req.body)
+        })
+        .then(() => res.json('Player Added'))
+        .catch(err => res.status(400).json('Error: ' + err))
+}
