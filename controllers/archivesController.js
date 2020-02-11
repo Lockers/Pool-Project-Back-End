94fc0752df21f6a7a33c0a7dd079aa63222a47ee
Matt Locklin <mattlocklin@live.co.uk>
2020-02-11 00:06:16 +0000

    fixed easy archive

--- controllers/archivesController.js
+++ controllers/archivesController.js
@@ -1,7 +1,7 @@
 const Archive = require('../models/archives.model');
 
 exports.createArchives = (req, res, next) => {
-    Archive.find().sort({ date: 'desc' })
+    Archive.create(req.body)
         .then(Archive => res.json(Archive))
         .catch(err => res.status(400).json('Error: ' + err))
 }
